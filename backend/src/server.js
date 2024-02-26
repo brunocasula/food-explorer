cc09214d053e4dadbec7607cd57f561a40d4c631
brunocasula <brunocasula@gmail.com>
2024-02-25 22:22:17 -0300

    Add origin cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,8 @@ migrationsRun();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  // origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["https://foodexplorer-bc.netlify.app", "http://localhost:5173", "http://127.0.0.1:5173"],
   credentials: true,
 }));
 
